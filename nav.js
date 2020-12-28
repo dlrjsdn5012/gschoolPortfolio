21c57dfbfe25704380dd63d8057201357d318e9d
건우 <apple@geon-uui-MacBookPro.local>
2020-12-29 01:06:49 +0900

    update icons function

--- nav.js
+++ nav.js
@@ -6,3 +6,15 @@ toggleBtn.addEventListener('click', () => {
   menu.classList.toggle('active');
   icons.classList.toggle('active');
 });
+
+document.querySelector(".copyMail").addEventListener("click", function() {
+  swal("Success!", "클립보드에 메일 주소가 복사되었습니다!", "success");
+
+  var tempElem = document.createElement("textarea");
+  tempElem.value = "dlrjsdn5012@naver.com";
+  document.body.appendChild(tempElem);
+
+  tempElem.select();
+  document.execCommand("copy");
+  document.body.removeChild(tempElem);
+});
